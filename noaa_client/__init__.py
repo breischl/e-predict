08e5f919fc28d2e16cd76676c47c8f7b39f07f3c
BrianReischl <breischl@gmail.com>
2022-10-11 06:50:32 -0600

    Initial version of NOAA client

---
+++ noaa_client/__init__.py
@@ -0,0 +1,9 @@
+import os
+import warnings
+from typing import Optional
+
+class NoaaForecastClient:
+    """
+    Python wrapper for the NOAA Forecast weather API: https://www.weather.gov/documentation/services-web-api
+    
+    """
