e5ba6ed49f9698cd9b714b36ca78cdf0f8d01065
BrianReischl <breischl@gmail.com>
2022-10-23 09:52:44 -0600

    Cleaning up some repetition/magic in data download script

--- download_historical_data.py
+++ download_historical_data.py
@@ -8,13 +8,59 @@ from json_encoder import json
 import eia.eia_client as eia
 
 
-def download_historical_data():
-    ###################################
-    # Configs
-    ###################################
-    DATA_DIR = "historical_data"
+def download_historical_data(electric_data_dir: str, weather_data_dir: str, weather_station_ids: list[str], eia_respondent: str = "PSCO"):
 
-    PSCO_STATION_IDS = [
+    GHCND_BASE_URL = "https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/all"
+
+    dotenv.load_dotenv()
+
+    ###################################
+    # Download EIA electric usage data
+    ###################################
+    start_date = date(2015, 1, 1)
+    end_date = date.today()
+
+    print("Downloading EIA demand data...")
+    usage_data = eia.get_electric_demand_hourly(start_date, end_date, respondent=eia_respondent)
+
+    if not os.path.exists(electric_data_dir):
+        os.makedirs(electric_data_dir)
+
+    eia_file = os.path.join(electric_data_dir, f"{eia_respondent}.json")
+    print(f"Writing EIA demand data to {eia_file}...")
+    with open(eia_file, "w", encoding="utf-8") as f:
+        json.dump(usage_data, f, indent=4, allow_nan=True)
+
+    print("Completed downloading EIA demand data")
+
+    #########################
+    # Download GHCN-d Weather Data
+    #########################
+
+    print("Downloading historical weather data from GHCN-d...")
+
+    if not os.path.exists(weather_data_dir):
+        os.makedirs(weather_data_dir)
+
+    for station_id in weather_station_ids:
+        dly_name = f"{station_id}.dly"
+        dly_file_path = os.path.join(weather_data_dir, dly_name)
+        station_url = f"{GHCND_BASE_URL}/{dly_name}"
+
+        print(f"Downloading {station_url} to {dly_file_path}...")
+
+        resp = requests.get(url=station_url, timeout=120)
+        with open(dly_file_path, "w", encoding="utf-8") as f:
+            f.write(resp.text)
+
+    print("Finished downloading data")
+
+
+if __name__ == "__main__":
+    HISTORICAL_DATA_DIR = os.path.abspath("./historical_data")
+    ELECTRIC_DATA_DIR = os.path.join(HISTORICAL_DATA_DIR, "electric_data")
+    WEATHER_DATA_DIR = os.path.join(HISTORICAL_DATA_DIR, "weather_station_data")
+    WEATHER_STATION_IDS = [
         "USW00023066",  # Grand Junction Walker Field
         "USC00053553",  # Greeley UNC
         "USC00053005",  # Ft Collins
@@ -24,52 +70,4 @@ def download_historical_data():
         "USW00023061"  # Alamosa
     ]
 
-    GHCND_BASE_URL = "https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/all"
-
-    EIA_RESPONDENT = "PSCO"
-
-    dotenv.load_dotenv()
-
-    ###################################
-    # Download EIA electric usage data
-    ###################################
-    print(f"Downloading historical data to {DATA_DIR}")
-
-    start_date = date(2015, 1, 1)
-    end_date = date.today()
-
-    print("Downloading EIA demand data...")
-    usage_data = eia.get_electric_demand_hourly(start_date, end_date, respondent=EIA_RESPONDENT)
-
-    if not os.path.exists(DATA_DIR):
-        os.mkdir(DATA_DIR)
-
-    EIA_FILE = os.path.join(DATA_DIR, "electric_data", f"{EIA_RESPONDENT}.json")
-    print(f"Writing EIA demand data to {EIA_FILE}...")
-    with open(EIA_FILE, "w", encoding="utf-8") as f:
-        json.dump(usage_data, f, indent=4, allow_nan=True)
-
-    print("Completed downloading EIA demand data")
-
-    #########################
-    # Download GHCN-d Weather Data
-    #########################
-
-    DLY_FILE_DIR = os.path.join(DATA_DIR, "weather_station_data")
-    if not os.path.exists(DLY_FILE_DIR):
-        os.mkdir(DLY_FILE_DIR)
-
-    for station_id in PSCO_STATION_IDS:
-        dly_name = f"{station_id}.dly"
-        dly_file_path = os.path.join(DLY_FILE_DIR, dly_name)
-        station_url = f"{GHCND_BASE_URL}/{dly_name}"
-
-        print(f"Downloading {station_url} to {dly_file_path}...")
-
-        resp = requests.get(url=station_url, timeout=120)
-        with open(dly_file_path, "w", encoding="utf-8") as f:
-            f.write(resp.text)
-
-
-if __name__ == "main":
-    download_historical_data()
+    download_historical_data(ELECTRIC_DATA_DIR, WEATHER_DATA_DIR, WEATHER_STATION_IDS, eia_respondent="PSCO")
